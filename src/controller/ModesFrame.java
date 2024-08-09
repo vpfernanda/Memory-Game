b3547c1234aa2123347363ccb6f99be5c7ff9655
vpfernanda <fernandavpagano@gmail.com>
2024-08-09 12:25:25 -0300

    Modifications on ModesFrame.java -> especially on playerNameTextField.

--- src/controller/ModesFrame.java
+++ src/controller/ModesFrame.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.ArrayList;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
@@ -15,20 +19,25 @@ public class ModesFrame extends JFrame{
     private  ArrayList<String> themes;
     private JLabel modesLabel;
     private JLabel themesLabel;
-    //private ButtonGroup modesButtons;
-    //private ButtonGroup themesButtons;
     private JButton okButton, closeWindowButton;
-    private JTextField playerName;
+    private JTextField playerNameTextField;
+    private String username;
+    private int chosenMode;
+    private int chosenTheme;
+
     
     public ModesFrame(){
         this.setTitle("Escolher um modo de jogo");
+        this.setMaximumSize(new Dimension(350, 350));
+        this.setMinimumSize(new Dimension(350, 350));
+        this.setPreferredSize(new Dimension(350, 350));
         initModesArray();
         initThemesArray(); 
         modesLabel = new JLabel("Escolha o nível de dificuldade:");
         themesLabel = new JLabel("Escolha o tema do seu jogo:");
         this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        this.playerName = new JTextField("Como você quer ser chamado?",50);
-        this.add(playerName);
+        this.add(new JLabel("Como você quer ser chamado?"));
+        this.add(initPlayerNameField());
         this.add(modesLabel);
         initRadioButtonsGroup(modes);
         this.add(themesLabel);
@@ -37,10 +46,12 @@ public class ModesFrame extends JFrame{
         closeWindowButton = new JButton("Fechar jogo");
         this.add(okButton);
         this.add(closeWindowButton);
-        this.setPreferredSize(new Dimension(350, 400));
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        this.playerNameTextField.setMinimumSize(new Dimension(playerNameTextField.getParent().getWidth(),20));
+        this.playerNameTextField.setMaximumSize(new Dimension(playerNameTextField.getParent().getWidth(),20));
+        this.playerNameTextField.setPreferredSize(new Dimension(playerNameTextField.getParent().getWidth(),20));
     }
 
     private void initModesArray(){
@@ -67,6 +78,36 @@ public class ModesFrame extends JFrame{
             buttonGroup.add(radioButtonArray[c]);
             this.add(radioButtonArray[c]);
         }
+    }
+
+    private JTextField initPlayerNameField(){
+        this.playerNameTextField = new JTextField("Digite seu nome",0);
+        this.playerNameTextField.setForeground(Color.gray);
+        this.playerNameTextField.setFont(playerNameTextField.getFont().deriveFont(Font.ITALIC));
+        playerNameTextField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // Remove placeholder text when focus is gained
+                if (playerNameTextField.getText().equals("Digite seu nome")) {
+                    playerNameTextField.setText("");
+                    playerNameTextField.setForeground(Color.BLACK);
+                    playerNameTextField.setFont(playerNameTextField.getFont().deriveFont(Font.PLAIN));
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(playerNameTextField.getText().isEmpty())
+                    playerNameTextField.setText("Digite seu nome");
+                    playerNameTextField.setForeground(Color.gray);
+                    playerNameTextField.setFont(getFont().deriveFont(Font.ITALIC));
+            }
+        });
+        return playerNameTextField;
+    } 
+
+    private void initOkCancelButtonsContainer(){
+        //TODO: inicializar os botões ok e cancel em um container (Panel?) que tenha layout BOX, com X_AXIS
     }
     
     
